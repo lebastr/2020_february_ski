7e4433b8ba512fbb9ee2acbc00a78618c1c1f2e7
Alexander Lebedev <lebastr@gmail.com>
2020-01-28 18:25:45 +0300

    добавил ракладку Саши П. и переделал ее

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-log.txt: estimate_weight_balance.py products.py stuff.py
+log.txt: estimate_weight_balance.py shared_equipment.py
 	./estimate_weight_balance.py > log.txt
--- planner/equipment.py
+++ planner/equipment.py
@@ -37,14 +37,23 @@ def equipment_planning(SharedEquipment):
 
     print("Общий вес: %.0f" % total_weight)
 
+    males = list(filter(lambda m: m.male, Members.values()))
+    females = list(filter(lambda m: not m.male, Members.values()))
+    
     cur_weight = np.sum([m.equipment_weight() for m in Members.values()])
-    avg_man_weight = np.mean([m.equipment_weight() for m in Members.values() if m.male])
-    avg_woman_weight = np.mean([m.equipment_weight() for m in Members.values() if not m.male])
+    total_man_weight = np.sum([m.equipment_weight() for m in males])
+    avg_man_weight = total_man_weight / len(males)
+    avg_woman_weight = np.mean([m.equipment_weight() for m in females])
 
+    normal_man_weight = (total_man_weight - WomanSharedEquipWeightNorm*len(females)) / len(males)
+    assert normal_man_weight >= WomanSharedEquipWeightNorm
+    
     print("Средний вес на мужчину: %.0f" % avg_man_weight)
+    print("Норма на мужчину: %.0f" % normal_man_weight)
     print("Средний вес на женщину: %.0f\n" % avg_woman_weight)
+    print("Норма на женщину: %.0f\n" % WomanSharedEquipWeightNorm)
     print()
-    print_overweight(Members, Person.equipment_weight, avg_man_weight, WomanSharedEquipWeightNorm, caption='Перевес по снаряжению')
+    print_overweight(Members, Person.equipment_weight, normal_man_weight, WomanSharedEquipWeightNorm, caption='Перевес по снаряжени')
 
     print("\nОстаток: %.0f\n" % (total_weight - cur_weight))
     table = pd.DataFrame(data=tbl)
